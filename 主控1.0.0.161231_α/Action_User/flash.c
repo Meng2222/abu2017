5659fc77e129da37b977815e5e9a454993fa4261
ZENITH-DZC\neodz <tangsquirrel@gmail.com>
2017-03-21 13:45:09 +0800

    完成了向FLASH写PosDatabase，两个接口在flash.h中有声明

--- 主控1.0.0.161231_α/Action_User/flash.c
+++ 主控1.0.0.161231_α/Action_User/flash.c
@@ -1,9 +1,9 @@
 /**
  ******************************************************************************
- * @file
- * @author  lxy
+ * @file     flash.c
+ * @author   lxy DZC
  * @version
- * @date
+ * @date     2017/03/21
  * @brief
  ******************************************************************************
  * @attention
@@ -22,8 +22,8 @@
 
 /* Private  typedef -----------------------------------------------------------*/
 /* Private  define ------------------------------------------------------------*/
-#define FLASH_USER_ADDRESS 0x08040000   //FLASH起始地址
-
+#define FLASH_USER_ADDRESS 0x08040000   //FLASH起始地址 STM32F407VET6第6扇区的起始地址
+//#define WRITE_DATABASE_IN_FLASH_AT_BEGINNING 1
 //fix me 以下两个宏定义只是为了测试
 //#define TempTable_max	200
 //#define TempTable_min	100
@@ -68,7 +68,7 @@ static void Flash_Encryp(void)
 /* Exported functions ---------------------------------------------------------*/
 /**
  * @brief  向FLASH中写入数据
- *
+ * @note   正常来说使用3.3V时可以按照32位写入 
  * @param  data: 存储数据的指针
  * @param  len:  写入的数据量，单位：byte
  * @retval None
@@ -111,34 +111,28 @@ void FlashWriteFloatArr(float *data, uint32_t len)
 }
 
 /**
- * @brief  向FLASH中写入3个浮点数数组
- * @param  data: 浮点数数组的指针
- * @param  len:  浮点数数据量，单位：4byte
+ * @brief  向FLASH中写入3个枪的位置数据
+ * @param  None
  * @retval None
  */
-void FlashWriteGunPosData(float *data1,	uint32_t len1,
-		float *data2, uint32_t len2,
-		float *data3, uint32_t len3)
+void FlashWriteGunPosData(void)
 {
 	uint32_t count;
 	FLASH_Unlock();
 	FLASH_ClearFlag(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR);
 	FLASH_EraseSector(FLASH_Sector_6, VoltageRange_3);
 	FLASH_WaitForLastOperation();
-	for(count = 0; count < len1; count++)
+	for(count = 0; count < LEFTGUNPOSDATABASE_FLOAT_NUM; count++)
 	{
-		FLASH_ProgramHalfWord(FLASH_USER_ADDRESS + 4 * count, *(uint16_t *)(data1 + count));
-		FLASH_ProgramHalfWord(FLASH_USER_ADDRESS + 4 * count + 2, *((uint16_t *)(data1 + count) + 1));
+		FLASH_ProgramWord(FLASH_USER_ADDRESS + 4 * count, *(uint32_t *)((float *)gLeftGunPosDatabase + count));
 	}
-	for(; count < len2; count++)
+	for(; count < RIGHTGUNPOSDATABASE_FLOAT_NUM; count++)
 	{
-		FLASH_ProgramHalfWord(FLASH_USER_ADDRESS + 4 * count, *(uint16_t *)(data2 + count));
-		FLASH_ProgramHalfWord(FLASH_USER_ADDRESS + 4 * count + 2, *((uint16_t *)(data2 + count) + 1));
+		FLASH_ProgramHalfWord(FLASH_USER_ADDRESS + 4 * count, *(uint32_t *)((float *)gRightGunPosDatabase + count));
 	}
-	for(; count < len3; count++)
+	for(; count < RIGHTGUNPOSDATABASE_FLOAT_NUM; count++)
 	{
-		FLASH_ProgramHalfWord(FLASH_USER_ADDRESS + 4 * count, *(uint16_t *)(data3 + count));
-		FLASH_ProgramHalfWord(FLASH_USER_ADDRESS + 4 * count + 2, *((uint16_t *)(data3 + count) + 1));
+		FLASH_ProgramHalfWord(FLASH_USER_ADDRESS + 4 * count, *(uint32_t *)((float *)gUpperGunPosDatabase + count));
 	}
 	FLASH_Lock();
 }
@@ -165,27 +159,15 @@ void Flash_Zero(uint32_t len)
 }
 
 
-/**
- * @brief  从FLASH中读取数据
- *
- * @param  data :  存储数据的指针
- * @param  len  :  读取的数据量，单位：byte
- * @retval None
- */
-void Flash_Read(uint8_t *data,uint32_t len)
-{
-	uint32_t i;
-	for(i=0;i<len;i++)
-		*(data+i)= *((uint8_t *)(FLASH_USER_ADDRESS+i));
-}
+
 
 /**
- * @brief  从FLASH中读取数据
- *
- * @param  data :  存储数据的指针
- * @param  len  :  读取的数据量，单位：byte
- * @retval None
- */
+  * @brief  从FLASH中读取数据
+  *
+  * @param  data :  存储数据的指针
+  * @param  len  :  读取的数据量，单位：byte
+  * @retval None
+  */
 void Flash_ReadFloat(float *data, uint32_t len)
 {
 	uint32_t i;
@@ -193,94 +175,31 @@ void Flash_ReadFloat(float *data, uint32_t len)
 		*(data+i)= *((float *)(FLASH_USER_ADDRESS + 4*i));
 }
 
-///**
-//  * @brief  初始化FLASH
-//	*
-//  * @param  None
-//  * @retval None
-//  */
-//void Flash_Init(void)
-//{
-//	/* 读取FLASH中保存的数据，并将其存到RAM里 */
-//	Flash_Read(flashdata,160*(TempTable_max-TempTable_min));
-//	/* 一个温度可以分成10等分，每0.1度对应两个芯片，
-//	  3个角速度，1个计数值，一共16字节 */
-//	/* 分割数据段，将零漂值与计数值分开 */
-//	Result=(float *)flashdata;
-//	countnum=(uint32_t *)(flashdata)+30*(TempTable_max-TempTable_min);//类上
 
-//	/* 保护Flash数据 */
-//	Flash_Encryp();
-//}
 
 /**
- * @brief  初始化FLASH
- *
- * @param  None
- * @retval None
- */
+  * @brief  初始化FLASH
+  * @note   如果定义了WRITE_DATABASE_IN_FLASH_AT_BEGINNING
+  *				则写入数据，FLASH中存储的原有数据全部将被擦除,并且被database.c中的数据代替
+  *			如果没有定义WRITE_DATABASE_IN_FLASH_AT_BEGINNING，
+  *				则database.c中的数据将无效，三个数组中的数据将会被FLASH中以前记录下来的数据代替
+  * @param  None
+  * @retval None
+  */
 void Flash_Init(void)
 {
+#ifdef WRITE_DATABASE_IN_FLASH_AT_BEGINNING
+	FlashWriteGunPosData();
+#endif
 	/* 读取FLASH中保存的数据，并将其存到RAM里 */
-	Flash_ReadFloat((float *)gTestGunPosDatabase, LEFTGUNPOSDATABASE_FLOAT_NUM);
+	Flash_ReadFloat((float *)gLeftGunPosDatabase, LEFTGUNPOSDATABASE_FLOAT_NUM);
 	Flash_ReadFloat((float *)gRightGunPosDatabase, RIGHTGUNPOSDATABASE_FLOAT_NUM);
 	Flash_ReadFloat((float *)gUpperGunPosDatabase, UPPERGUNPOSDATABASE_FLOAT_NUM);
-
-	//	Result=(float *)gGunPosDatabaseFromFlash;
-	//	countnum = (uint32_t *)(gGunPosDatabaseFromFlash) + len;//类上
 
 	/* 保护Flash数据 */
 	Flash_Encryp();
 }
-///**
-//  * @brief  获取从FLASH里得到的数据的指针
-//	*
-//  * @param  None
-//  * @retval flashdata : 指向flash数据的指针
-//  */
-//uint8_t *GetFlashArr(void)
-//{
-//	return flashdata;
-//}
-/**
- * @brief  从FLASH里得到的数据分两个部分，第一部分为数据区，第二部分为
- *		 计数区，该函数用于获得数据区的指针
- * @param  None
- * @retval Result : 指向flash数据区的指针
- */
-float *GetResultArr(void)
-{
-	return Result;
-}
-/**
- * @brief  从FLASH里得到的数据分两个部分，第一部分为数据区，第二部分为
- *		 计数区，该函数用于获得计数区的指针
- * @param  None
- * @retval Result : 指向flash计数区的指针
- */
-uint32_t *GetCountArr(void)
-{
-	return countnum;
-}
-/**
- * @brief  获得FLASH是否需要更新的标志位
- *
- * @param  None
- * @retval flag : flash更新的标志位
- */
-uint8_t GetFlashUpdataFlag(void)
-{
-	return flag;
-}
-/**
- * @brief  设置FLASH更新标志位的值
- *
- * @param  val  : 赋给更新标志位的值
- * @retval None
- */
-void  SetFlashUpdateFlag(uint8_t val)
-{
-	flag=val;
-}
+
+
 /************************ (C) COPYRIGHT 2016 ACTION *****END OF FILE****/
 
--- 主控1.0.0.161231_α/Action_User/flash.h
+++ 主控1.0.0.161231_α/Action_User/flash.h
@@ -34,6 +34,15 @@ extern "C"
 void Flash_Write(uint8_t *data,uint32_t len);
 void Flash_Zero(uint32_t len);
 void Flash_Read(uint8_t *data,uint32_t len);
+/**
+ * @brief  初始化FLASH
+ * @note   如果定义了WRITE_DATABASE_IN_FLASH_AT_BEGINNING
+ *				则写入数据，FLASH中存储的原有数据全部将被擦除,并且被database.c中的数据代替
+ *			如果没有定义WRITE_DATABASE_IN_FLASH_AT_BEGINNING，
+ *				则database.c中的数据将无效，三个数组中的数据将会被FLASH中以前记录下来的数据代替
+ * @param  None
+ * @retval None
+ */
 void Flash_Init(void);
 uint8_t  *GetFlashArr(void);
 float	*GetResultArr(void);
@@ -43,9 +52,25 @@ void	 SetFlashUpdateFlag(uint8_t val);
 
 
 void FlashWriteFloatArr(float *data, uint32_t len);
-void FlashWriteGunPosData(float *data1,	uint32_t len1,
-							float *data2, uint32_t len2,
-							float *data3, uint32_t len3);
+
+
+/**
+  * @brief  向FLASH中写入3个枪的位置数据
+  * @note   此函数将会使用大概一秒钟的时间使用时最好慎重考虑
+  * @param  None
+  * @retval None
+  */
+void FlashWriteGunPosData(void);
+/**
+ * @brief  初始化FLASH
+ * @note   如果定义了WRITE_DATABASE_IN_FLASH_AT_BEGINNING
+ *				则写入数据，FLASH中存储的原有数据全部将被擦除,并且被database.c中的数据代替
+ *			如果没有定义WRITE_DATABASE_IN_FLASH_AT_BEGINNING，
+ *				则database.c中的数据将无效，三个数组中的数据将会被FLASH中以前记录下来的数据代替
+ * @param  None
+ * @retval None
+ */
+void FlashWriteGunPosData(void);
 
 #ifdef __cplusplus  //为C提供接口
 }
