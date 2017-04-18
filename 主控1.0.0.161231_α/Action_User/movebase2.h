09bb2e376930bb00c977163c4f1ec76773f1176f
Meng2222 <Dreamer_Meng2@163.com>
2017-04-18 22:26:34 +0800

    提交movebase2.h

--- 主控1.0.0.161231_α/Action_User/movebase2.h
+++ 主控1.0.0.161231_α/Action_User/movebase2.h
@@ -24,11 +24,8 @@
 
 /* Includes ------------------------------------------------------------------*/
 
-
+#include "stdint.h"
 #include "robot.h"
-#include "movebase.h"
-
-
 /* Exported types ------------------------------------------------------------*/
 
 /** 
@@ -41,11 +38,34 @@
 
 
 
-/** @defgroup 
+
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+/** @defgroup Get_Kinematic_Information
+  * @brief
   * @{
   */
 
+/**
+  * @brief	设定X坐标的矫正量
+  * @param	theoreticalX 理论坐标
+  * @param	actualX 实际坐标
+  * @retval	None
+  */
+void SetXCorrection(float theoreticalX);
+void SetDispCorrection(float theoreticalDisp);
 
+/**
+  * @brief  UpdateMoveBaseData
+  * @note
+  * @param  angle: data that will update the angle in gRobot.movebase.actualPos
+  * @param  posX: data that will update the X pos in gRobot.movebase.actualPos
+  * @param  posY: data that will update the Y pos in gRobot.movebase.actualPos
+  * @retval None
+  */
+void UpdateXYAngle(float angle, float posX, float posY);
+void UpdateKenimaticInfo(void);
 
 
 /**
@@ -53,9 +73,43 @@
   */
 
 
-/* Exported macro ------------------------------------------------------------*/
-/* Exported functions --------------------------------------------------------*/
 
+
+/** @defgroup Record_Walk_Track
+  * @brief
+  * @{
+  */
+
+/**
+  * @brief  Record Walking Track
+  * @note   X, Y, angle, Journey, Displacement must be updated before calling this function
+  * @param	None
+  * @retval
+  */
+void RecordWalkingTrack(void);
+
+/**
+  * @}
+  */
+
+
+
+/** @defgroup 
+  * @brief 
+  * @{
+  */
+
+/**
+  * @brief	
+  * @note	This function is supposed to be called every 10ms
+  * @param	None
+  * @retval	None
+  */
+void MovebaseRun(float finalDisp, float targetVel, float targetAcc, float targetDec, uint8_t enable);
+
+/**
+  * @}
+  */
 
 
 #ifdef __cplusplus
